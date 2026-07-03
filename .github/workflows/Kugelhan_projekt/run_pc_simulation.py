9d265fac5641d85ac3be8a1cf312010fd9f68784
Bastian Kadlubski <299089405+Alucard-python-code@users.noreply.github.com>
2026-07-03 17:09:58 +0200

    change

--- .github/workflows/Kugelhan_projekt/run_pc_simulation.py
+++
@@ -1,7 +0,0 @@
-# run_pc_simulation.py
-# 1. Hardware-Simulation laden (Muss als ALLERERSTES passieren!)
-import hardware_simulator
-
-# 2. Das originale Hauptprogramm importieren und starten
-print("[Simulator] Starte das originale Hauptprogramm (main.py)...")
-import main
