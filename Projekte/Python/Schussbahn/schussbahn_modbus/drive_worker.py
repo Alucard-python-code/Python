d469a200bf70577ca811b9ce5db7a35fdeac1918
Bastian Kadlubski <Alucard-python-code@users.noreply.github.com>
2026-08-15 10:52:56 +0200

    watchdog wieder standart

--- Projekte/Python/Schussbahn/schussbahn_modbus/drive_worker.py
+++ Projekte/Python/Schussbahn/schussbahn_modbus/drive_worker.py
@@ -122,7 +122,7 @@ class DriveThread(QThread):
 
                 end_time_schnell = time.time() + self.times["Beschuss Schnell"]
                 while time.time() < end_time_schnell:
-                    self.check_watchdog()
+                    check_watchdog()
                     self.check_inputs_during_flight()
                     time.sleep(0.05)
 
@@ -131,7 +131,7 @@ class DriveThread(QThread):
 
                 end_time_langsam = time.time() + self.times["Beschuss Langsam"]
                 while time.time() < end_time_langsam:
-                    self.check_watchdog()
+                    check_watchdog()
                     self.check_inputs_during_flight()
                     time.sleep(0.05)
 
@@ -149,9 +149,9 @@ class DriveThread(QThread):
                 self.write_hardware_coil(3, True)
                 self.status_signal.emit("Wertung: Schnellphase")
 
-                end_time_schnell = time.time() + self.times.get("Wertung Schnell", 2.5)
+                end_time_schnell = time.time() + self.times.get("Wertung Schnell")
                 while time.time() < end_time_schnell:
-                    self.check_watchdog()
+                    check_watchdog()
                     self.check_inputs_during_flight()
 
                     if isinstance(self.latest_inputs, list) and len(self.latest_inputs) > 1 and self.latest_inputs[1] == True:
@@ -163,7 +163,7 @@ class DriveThread(QThread):
                 self.status_signal.emit("Wertung: Langsamphase")
 
                 while self._is_running:
-                    self.check_watchdog()
+                    check_watchdog()
                     self.check_inputs_during_flight()
 
                     if isinstance(self.latest_inputs, list) and len(self.latest_inputs) > 1:
@@ -184,7 +184,7 @@ class DriveThread(QThread):
                 self.write_hardware_coil(2, True)
 
                 while self._is_running:
-                    self.check_watchdog()
+                    check_watchdog()
                     self.check_inputs_during_flight()
 
                     if isinstance(self.latest_inputs, list) and len(self.latest_inputs) > 1:
